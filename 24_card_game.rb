e293f08a17acfe6a75eb3b8814e4f5322cc7737f
Danylo Denysov <danyldezwaan@hotmail.com>
2018-10-02 09:44:13 +1000

    resolve conflicts

--- 24_card_game.rb
+++ 24_card_game.rb
@@ -1,13 +1,46 @@
 class Card
+    include Comparable
+    attr_accessor :suit, :rank
     def initialize(suit, rank)
         # Create a new card
+        @suit = suit
+        @rank = rank
     end
     
     def face_card?
         # Is the card a face card (above 10)?
+        return @rank > 10
     end 
     
     def to_s
-        # Return human readable card
+        string_builder = ""
+        case @rank
+        when 1
+            string_builder += "Ace" 
+        when 11
+            string_builder += "Jack"
+        when 12
+            string_builder += "Queen"
+        when 13
+            string_builder += "King"
+        else
+            string_builder += "#{@rank}"
+        end
+        
+        string_builder += " of "
+        string_builder += "#{@suit}".capitalize
+        
+        return string_builder
+    end
+    
+    def <=>(other)
+        @rank <=> other.rank
     end
 end
+
+
+queen_of_spades = Card.new(:spades, 12)
+ten_of_clubs = Card.new(:clubs, 10)
+ace_of_diamonds = Card.new(:diamonds, 1)
+
+p queen_of_spades > ten_of_clubs
